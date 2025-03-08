4eed388db7cc8af82e792409bef9f526715a32c4
jamestexas <18285880+jamestexas@users.noreply.github.com>
2025-03-07 19:00:16 -0600

    chore: remove old 'memoryweave/retrieval' code

--- memoryweave/retrieval/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Retrieval component implementations for MemoryWeave.
-
-This package contains implementations of memory retrieval components,
-including retrieval strategies and related utilities.
-"""
